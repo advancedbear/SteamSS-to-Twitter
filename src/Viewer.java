c82a165a51d10e24875fa2b886bdd96d4e97ff3e
advanced_bear <advanced.bear@gmail.com>
2015-08-27 22:29:44 +0900

    スクロールペインを追加し、アクションリスナーを設定。

--- src/Viewer.java
+++ src/Viewer.java
@@ -1,26 +1,46 @@
 import java.awt.EventQueue;
-import java.io.IOException;
+import java.awt.event.*;
+import java.io.*;
 
 import javax.swing.JFrame;
 import javax.swing.JComboBox;
 import javax.swing.JSplitPane;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 import javax.swing.border.LineBorder;
+
+import twitter4j.TwitterException;
+import twitter4j.auth.*;
+
 import java.awt.Color;
 import javax.swing.border.BevelBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.JButton;
 
-public class Viewer {
+public class Viewer extends JFrame implements ActionListener {
 
 	private JFrame frmSteamScreenshotUploader;
 
-	/**
-	 * Launch the application.
-	 */
+	JComboBox gameTitle = new JComboBox();
+	JList list = new JList();
+	JLabel imagePlace = new JLabel("New label");
+	JLabel accountName = new JLabel("Please login first.");
+	JButton auth = new JButton("Login");
+	JTextArea text = new JTextArea();
+	JButton submit = new JButton("Tweet");
+
+	File token = new File("token");
+	boolean loading = false;
+	boolean authentication = false;
+	
+	Tweet t = new Tweet();
+	Parse p = new Parse();
+	
 	public static void main(String[] args) {
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -31,14 +51,6 @@ public class Viewer {
 				}
 			}
 		});
-		
-		Parse p = new Parse();
-		try {
-			System.out.println(p.getTitle(7));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 
 	/**
@@ -58,34 +70,96 @@ public class Viewer {
 		frmSteamScreenshotUploader.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmSteamScreenshotUploader.getContentPane().setLayout(null);
 		
-		JComboBox gameTitle = new JComboBox();
 		gameTitle.setBounds(12, 10, 228, 19);
 		frmSteamScreenshotUploader.getContentPane().add(gameTitle);
 		
-		JList list = new JList();
 		list.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		list.setBounds(12, 39, 228, 392);
 		frmSteamScreenshotUploader.getContentPane().add(list);
 		
-		JLabel imagePlace = new JLabel("New label");
 		imagePlace.setBounds(252, 10, 360, 270);
 		frmSteamScreenshotUploader.getContentPane().add(imagePlace);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(252, 319, 360, 64);
-		frmSteamScreenshotUploader.getContentPane().add(textArea);
-		
-		JButton submit = new JButton("Tweet");
+		submit.setEnabled(false);
 		submit.setBounds(252, 393, 360, 38);
 		frmSteamScreenshotUploader.getContentPane().add(submit);
-		
-		JLabel accountName = new JLabel("New label");
+		submit.addActionListener(this);
+		submit.setActionCommand("tweet");
+
 		accountName.setBounds(252, 296, 257, 13);
 		frmSteamScreenshotUploader.getContentPane().add(accountName);
 		
-		JButton btnNewButton = new JButton("Login");
-		btnNewButton.setBounds(521, 292, 91, 21);
-		frmSteamScreenshotUploader.getContentPane().add(btnNewButton);
+		auth.setBounds(521, 292, 91, 21);
+		frmSteamScreenshotUploader.getContentPane().add(auth);
+		auth.addActionListener(this);
+		auth.setActionCommand("auth");
 		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(252, 319, 360, 64);
+		frmSteamScreenshotUploader.getContentPane().add(scrollPane);
+		scrollPane.setViewportView(text);
+		text.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+
+		if (loadAccessToken() != null) {
+			t.authorization(loadAccessToken());
+			loading = true;
+			try {
+				accountName.setText(t.twitter.getScreenName());
+			} catch (IllegalStateException e) {
+				e.printStackTrace();
+			} catch (TwitterException e) {
+				e.printStackTrace();
+			}
+			submit.setEnabled(true);
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("auth")) {
+			System.out.println("push auth key.");
+			t.TwitterAuth();
+			String value = JOptionPane.showInputDialog(this, "PINî‘çÜÇì¸óÕÇµÇƒâ∫Ç≥Ç¢");
+			if (value == null) {
+			} else {
+				t.authorization(value);
+				try {
+					accountName.setText("@"+t.twitter.getScreenName());
+					authentication = true;
+					storeAccessToken(t.accessToken);
+					submit.setEnabled(true);
+				} catch (TwitterException e1) {
+					e1.printStackTrace();
+				}
+			}
+		} else if (e.getActionCommand().equals("tweet")){
+		}
+	}
+	
+
+	private AccessToken loadAccessToken() {
+		ObjectInputStream is = null;
+		try {
+			is = new ObjectInputStream(new FileInputStream(token));
+			AccessToken accessToken = (AccessToken) is.readObject();
+			return accessToken;
+		} catch (IOException e) {
+			return null;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private void storeAccessToken(AccessToken accessToken) {
+		ObjectOutputStream os = null;
+		try {
+			if (!loading) {
+				os = new ObjectOutputStream(new FileOutputStream(token));
+				os.writeObject(accessToken);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
