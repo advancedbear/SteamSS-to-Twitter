824d595d53285bc889c89321cf4fb9c613fad252
advanced_bear <advanced.bear@gmail.com>
2015-08-28 00:10:14 +0900

    フォルダ探索を追加

---
+++ src/Parse.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class Parse {
+	String url, url2;
+	String[] folders = {"C:\\Program Files (x86)\\Steam\\userdata", "C:\\Program Files\\Steam\\userdata"};
+	File steamFolder, userFolder, gameFolder, image;
+	File[] userList, gameList, imageList;
+	
+	public Parse(){
+	    	url = "http://store.steampowered.com/app/";
+	    	url2 = "https://steamidfinder.ru/";
+	    	
+	    	for(int i=0; i<folders.length; i++){
+	    		steamFolder = new File(folders[i]);
+	    		if(steamFolder.exists()){
+	    			break;
+	    		}  else {
+	    			steamFolder = null;
+	    		}
+	    	}
+	    	
+	    	userList = steamFolder.listFiles();
+	    	userFolder = new File(userList[0].getAbsolutePath()+"\\760");
+	    	gameList = userFolder.listFiles();
+	}
+	
+	public String getTitle(int num) throws IOException {
+		url += num;
+        Document document = Jsoup.connect(url).get();
+        Elements elements = document.select("div.apphub_AppName");
+        return elements.text();
+	}
+	
+	public String getName(int num) throws IOException {
+		url2 += num;
+        Document document = Jsoup.connect(url2).get();
+        Elements elements = document.select("div.apphub_AppName");
+        return elements.text();
+	}
+}
