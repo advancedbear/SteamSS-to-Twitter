fbba304983e797730b662371ae9e640335789b04
advanced_bear <advanced.bear@gmail.com>
2015-08-27 21:25:03 +0900

    レイアウト決定

--- src/Viewer.java
+++ src/Viewer.java
@@ -1,10 +1,21 @@
 import java.awt.EventQueue;
+import java.io.IOException;
 
 import javax.swing.JFrame;
+import javax.swing.JComboBox;
+import javax.swing.JSplitPane;
+import javax.swing.JList;
+import javax.swing.JSeparator;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.border.BevelBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
 
 public class Viewer {
 
-	private JFrame frame;
+	private JFrame frmSteamScreenshotUploader;
 
 	/**
 	 * Launch the application.
@@ -14,12 +25,20 @@ public class Viewer {
 			public void run() {
 				try {
 					Viewer window = new Viewer();
-					window.frame.setVisible(true);
+					window.frmSteamScreenshotUploader.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
+		
+		Parse p = new Parse();
+		try {
+			System.out.println(p.getTitle(7));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -33,9 +52,40 @@ public class Viewer {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmSteamScreenshotUploader = new JFrame();
+		frmSteamScreenshotUploader.setTitle("Steam ScreenShot Uploader for Twitter");
+		frmSteamScreenshotUploader.setBounds(100, 100, 640, 480);
+		frmSteamScreenshotUploader.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmSteamScreenshotUploader.getContentPane().setLayout(null);
+		
+		JComboBox gameTitle = new JComboBox();
+		gameTitle.setBounds(12, 10, 228, 19);
+		frmSteamScreenshotUploader.getContentPane().add(gameTitle);
+		
+		JList list = new JList();
+		list.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		list.setBounds(12, 39, 228, 392);
+		frmSteamScreenshotUploader.getContentPane().add(list);
+		
+		JLabel imagePlace = new JLabel("New label");
+		imagePlace.setBounds(252, 10, 360, 270);
+		frmSteamScreenshotUploader.getContentPane().add(imagePlace);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(252, 319, 360, 64);
+		frmSteamScreenshotUploader.getContentPane().add(textArea);
+		
+		JButton submit = new JButton("Tweet");
+		submit.setBounds(252, 393, 360, 38);
+		frmSteamScreenshotUploader.getContentPane().add(submit);
+		
+		JLabel accountName = new JLabel("New label");
+		accountName.setBounds(252, 296, 257, 13);
+		frmSteamScreenshotUploader.getContentPane().add(accountName);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.setBounds(521, 292, 91, 21);
+		frmSteamScreenshotUploader.getContentPane().add(btnNewButton);
+		
 	}
-
 }
